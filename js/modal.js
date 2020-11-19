323f55ad723ba60272e49d83546c9ca9c5e1a7c2
William R <wiribasczky@gmail.com>
2020-11-19 15:57:49 -0300

    Criado classe reutilizável
    Problema/Proposito
    - Inserido assets(img)
    - Criado estilização CSS
    - Criada a classe(ES6) responsável pela reutilização
    - Criado arquivo index.html que contém inclusão
      de módulo para o desenvolvimento

---
+++ js/modal.js
@@ -0,0 +1,40 @@
+export default class Modal {
+    constructor(botaoAbrir, botaoFechar, containerModal) {
+      this.botaoAbrir = document.querySelector(botaoAbrir);
+      this.botaoFechar = document.querySelector(botaoFechar);
+      this.containerModal = document.querySelector(containerModal);
+  
+      // bind para this fazer referencia ao objeto da classe
+      this.eventToggleModal = this.eventToggleModal.bind(this);
+      this.clickForaModal = this.clickForaModal.bind(this);
+    }
+  
+    toggleModal() {
+      this.containerModal.classList.toggle('ativo');
+    }
+  
+    eventToggleModal(event) {
+      event.preventDefault();
+      this.toggleModal();
+    }
+  
+    clickForaModal(event) {
+      if (event.target === this.containerModal) {
+        this.toggleModal();
+      }
+    }
+  
+    addModalEvent() {
+      this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+      this.botaoFechar.addEventListener('click', this.eventToggleModal);
+      this.containerModal.addEventListener('click', this.clickForaModal);
+    }
+  
+    init() {
+      if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+        this.addModalEvent();
+      }
+      return this;
+    }
+  }
+  
